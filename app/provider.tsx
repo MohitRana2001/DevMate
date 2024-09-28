e1198db2696a611370ab86b4d68a9c31fc60b57b
MOHIT RANA <awesomemohit20@gmail.com>
2024-09-29 01:49:28 +0530

    working on displaying all of the rooms on the home page

--- app/provider.tsx
+++ app/provider.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
-export function Provider({ children } : { children: ReactNode }) {
+export function Providers({ children } : { children: ReactNode }) {
     return (
         <SessionProvider>
             <ThemeProvider
